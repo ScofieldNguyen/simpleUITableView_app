341b59216f78eadf6d121256af04d0648b3fec54
ScofielNguyen <scofieldnguyen96@gmail.com>
2016-10-03 13:02:37 +0700

    First commit

--- TableView_App/ViewController.swift
+++ TableView_App/ViewController.swift
@@ -8,8 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
+    var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats And Deli", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restaurantNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIndertifier = "Cell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIndertifier, for: indexPath)
+        cell.textLabel?.text = restaurantNames[(indexPath as NSIndexPath).row]
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
